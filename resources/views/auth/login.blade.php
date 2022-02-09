3adebc7cd5f15a14d06e69e6a24afc01c6fa94a0
Jessica Villa <suhkha92@gmail.com>
2022-02-08 18:36:24 -0600

    login form indentation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,34 +7,26 @@
     </div>
     <form method="POST" action="{{ route('login') }}">
         @csrf
-
         <div class="mt-8">
-            
             <div>
                 <label class="block" for="email">Email<label>
-                        <input type="email" id="email" name="email"         
-                            placeholder="Email"
-                            class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-teal-300"
-                            value="{{ old('email') }}" required autofocus>
+                <input type="email" id="email" name="email" placeholder="Email" class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-teal-300" value="{{ old('email') }}" required autofocus>
 
-                        @if ($errors->has('email'))
-                            <span class="invalid-feedback block mt-2 text-red-600">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback block mt-2 text-red-600">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
             </div>
             <div class="mt-4">
                 <label class="block">Password<label>
-                        <input type="password" id="password" name="password"
-                            placeholder="Password"
-                            class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-teal-300"
-                            required>
+                <input type="password" id="password" name="password" placeholder="Password" class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-teal-300" required>
 
-                        @if ($errors->has('password'))
-                            <span class="invalid-feedback block mt-2 text-red-600">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback block mt-2 text-red-600">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
             </div>
             <div class="flex items-baseline justify-between">
                 <button type="submit" class="px-6 py-2 mt-4 text-white bg-teal-400 rounded-lg hover:bg-teal-300">Login</button>
