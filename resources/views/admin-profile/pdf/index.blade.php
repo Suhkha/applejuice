c2b9d648c7fcbe0376cdd7f230ab553114421541
Jessica Villa <suhkha92@gmail.com>
2022-02-26 17:58:48 -0600

    Fix PDF, showing again

--- resources/views/admin-profile/pdf/index.blade.php
+++ resources/views/admin-profile/pdf/index.blade.php
@@ -9,7 +9,7 @@
                 </div>
             </div>
         </div>
-        <form action="{{ route('pdf.store', $id) }}" method="post" enctype="multipart/form-data" id="pdf-upload" class="dropzone bg-teal-100 px-9 py-9 w-full h-fit text-center border-dotted border-2 border-teal-400">
+        <form action="{{ route('pdf.store', $userId) }}" method="post" enctype="multipart/form-data" id="pdf-upload" class="dropzone bg-teal-100 px-9 py-9 w-full h-fit text-center border-dotted border-2 border-teal-400">
             @csrf
             <div>
                 <span class="text-gray-800 block mb-4">Arrastra el plan de alimentación (PDF) al recuadro o da clic al botón</h3>
